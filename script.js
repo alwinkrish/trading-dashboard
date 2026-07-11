dd2ccc8cc8c3c389f9442b6e9489b74faa4f4dae
alwinkrish <alwinchristopher25@gmail.com>
2026-07-11 15:58:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById("price").innerHTML = "NIFTY : 25120";
 
 document.getElementById("signal").innerHTML = "🟢 BUY CALL";
+document.getElementById("nifty").innerHTML = "Loading...";
+document.getElementById("banknifty").innerHTML = "Loading...";
+document.getElementById("vix").innerHTML = "Loading...";
+
+document.getElementById("signal").innerHTML = "🟡 WAIT";
+
+document.getElementById("support").innerHTML = "--";
+
+document.getElementById("resistance").innerHTML = "--";
